7faa302588d8c7411b93c7b918ce792f10e71941
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-09-17 14:22:03 -0300

    Add exercices in class, all need fix

---
+++ exercicios/para-sala/Calculadora.js
@@ -0,0 +1,20 @@
+class Calculadora {
+
+    static soma(a,b){
+    return a + b;
+    }
+    
+    static subtracao(a,b){
+    return a - b;
+    }
+    
+    static multiplicacao(a,b){
+    return a * b;
+    }
+    
+    static divisao (a,b){
+    return a/b;
+    }
+    }
+    
+    module.exports = {Calculadora} 
