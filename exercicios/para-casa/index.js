7e7046f052bb72940a1f1ba22e9557a3d6c20fd8
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-09-18 23:35:26 -0300

    Add notes about homework

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,19 +1,4 @@
-//Considere um objeto que represente uma conta bancária
-//Objeto deve ter: nome do titular, o saldo e o limite da conta
-
-//Checar se possui o nome do titular?
-//Checar saldo 
-//Checar o limite da conta
-
-//Tem que ser possível consulta de saldo 
-//Tem que ser possível saque
-//Tem que ser possível depósito
-
-//No caso de saque é necessário verificar se existe saldo suficiente para retirada
-//Caso o cliente não tenha saldo suficiente, mas possua limite disponível (e suficiente), o saque poderá ocorrer
-//Nesses como o acima casos o saldo do cliente ficará negativo após o saque.
-//O limite de uma conta pode ser reajustado (para mais e para menos) ou desativado.
-
+//Vide iNotes_Homework.txt
 
 const checkSaldo = (num1) => {
     if(num1 > 0){
