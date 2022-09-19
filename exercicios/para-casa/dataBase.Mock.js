5143efda866d0b72133afa69a4b4e62bf7215ebc
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-09-19 02:20:58 -0300

    Add update of dataBase Mock

--- exercicios/para-casa/dataBase.Mock.js
+++ exercicios/para-casa/dataBase.Mock.js
@@ -1,9 +1,20 @@
 const dataBase = [
-    {
-        "id": 1,
-        "client_name": "Débora Gomes",
-        "balance": 1000,
-        "limit": true,
-        "limit_value": 500
-    }
-]
+  {
+    id: 1,
+    client_name: "Débora Gomes",
+    balance: 1000,
+    limit: true,
+    limit_value: 500,
+  },
+  {
+    id: 2,
+    client_name: "Xênia Barreto",
+    balance: 0,
+    limit: false,
+    limit_value: 400,
+  }
+];
+
+module.exports = {
+     dataBase
+}
